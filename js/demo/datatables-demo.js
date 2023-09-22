0a060887586b67212dbf23a5a0c52986ee34322a
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2023-09-22 11:24:40 +0000

    Added :: Manage Utilities and Manager User Front End

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,12 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+$(document).ready(function() {
+  $('#role-dataTable').DataTable();
+});
+$(document).ready(function() {
+  $('#file-type-dataTable').DataTable();
+});
+$(document).ready(function() {
+  $('#backup-category-dataTable').DataTable();
+});
